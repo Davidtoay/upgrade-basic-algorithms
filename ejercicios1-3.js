969842f417918bdac8266ec454533db6ffd28f7f
David Torres Ayala <david.torresayala@hotmail.com>
2022-12-04 20:43:25 +0100

    iteracion 1 y 3

---
+++ ejercicios1-3.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var myfavoriteHero = "Hulk";
+var x = 50;
+var h = 5;
+var y = 10;
+var z = h + y;
+
+var mult = h*y;
+console.log(mult);
+
+var div = y / (y-h);
+console.log(div);
+
+var modulo = z%9;
+console.log(modulo);
+
+x == h*y;
+console.log(x);
